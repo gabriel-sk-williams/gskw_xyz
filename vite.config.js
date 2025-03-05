295d3443bc85771c3f69873f30ebc036adb708e4
Gabriel Williams <gabrielskwilliams@gmail.com>
2025-03-04 16:11:42 -0800

    public essays

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.md']
+  assetsInclude: ['**/*.md'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('essays/')) {
+            return 'essays';
+          }
+        }
+      }
+    }
+  }
 })
