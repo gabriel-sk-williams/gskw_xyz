985e3778d569eb0b1c6b4f2709132d0d15f82889
Gabriel Williams <gabrielskwilliams@gmail.com>
2025-06-20 17:57:44 -0700

    config

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -28,24 +28,17 @@ function BlogPost() {
 
   // Fetch markdown content
   useEffect(() => {
-
-    //if (post) {
-    //  import(/* @vite-ignore */post.path).then(res => {
-    //  fetch(post.path)
-    //    .then(response => response.text())
-    //    .then(text => setContent(text))
-    //    .catch(error => setError(error));
-    //  }
-    //}
-  if (post) {
-    fetchMarkdown(post.title).then(text => setContent(text));
-  }
+    if (post) {
+      fetchMarkdown(post.title).then(text => setContent(text));
+    }
   }, [post]);
+
 
   if (!post) {
     return <div>Post not found</div>;
   }
 
+  // Error
   if (error) {
     return <div>Error loading post: {error}</div>;
   }
@@ -63,25 +56,3 @@ function BlogPost() {
 }
 
 export default BlogPost;
-
-  /*
-  const importMarkdown = async () => {
-    try {
-      // Use dynamic import with relative path
-      const module = await import(`${post.path}`);
-      
-      // Fetch the markdown content
-      const response = await fetch(module.default);
-      
-      if (!response.ok) {
-        throw new Error('Failed to fetch markdown content');
-      }
-      
-      const text = await response.text();
-      setContent(text);
-    } catch (err) {
-      console.error('Error importing markdown:', err);
-      setError(err.message);
-    }
-  };
-  */
--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/essays/*.md', // Copy all .md files from src/essays
-          dest: 'essays' // Place them in dist/essays
-        }
-      ]
-    })
   ],
-  assetsInclude: ['**/*.md'],
 })
